f18048b1575d7ea77074b8b02fae12c726e093ce
gaurank <gaurangprabhu@gmail.com>
2017-12-06 18:12:48 +0530

    stringreverse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public  class Main {
 
     public static void main(String[] args) {
-        String s = ("knaruag");
+        String s = ("GaurankSharma");
         for( int i=0, j = (s.length())-1; i <= j; j-- ) {
             char c=s.charAt(j);
             System.out.print(c);
